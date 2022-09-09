08f1090625549c3c71b0f8417bd34fb736867e46
Kevin Dongmo <kevindongmo.f@gmail.com>
2022-09-09 15:52:12 -0700

    Add 6-size.c

---
+++ 6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a an long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a an long long: %lu bytes(s)\n", (unsigned long)sizeof(d));	
+	printf("Size of a an float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+
+	return (0);
+}
