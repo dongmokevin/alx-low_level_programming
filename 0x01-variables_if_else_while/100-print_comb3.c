c95992d494232bd57d9d58019cd936f27179ab74
Kevin Dongmo <kevindongmo.f@gmail.com>
2022-09-09 19:36:54 -0700

    Add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int y;
+	int skipper = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (y = skipper; y <= '9'; y++)
+		{
+			if (i != y)
+			{
+				putchar(i);
+				putchar(y);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		skipper++;
+	}
+	return (0);
+}
