42b99293a11b25cdde06233ee6b2bf7f9d876e5a
Kevin Dongmo <kevindongmo.f@gmail.com>
2022-09-09 19:11:14 -0700

    Update 9-print_com.b

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,15 @@ int main(void)
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
+	{
 		putchar(num);
-		putchar(" ,");
+
+		if (num != '9')
+		{
+			putchar(" ");
+			putchar(",");
+		}
+	}
 	putchar('\n');
 
 	return (0);
