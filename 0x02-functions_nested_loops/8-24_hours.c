619d079fb89cffcfaa61b3a5c7c5934258a406e1
Kevin Dongmo <kevindongmo.f@gmail.com>
2022-09-13 18:30:13 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int min, sec;
+
+	for (min = 0; min < 60; min++)
+	{
+		if (min < 10)
+			_putchar('0' + min);
+		else
+			_putchar(min);
+
+		_putchar(':');
+
+		for (sec = 0; sec < 60; sec++)
+		{
+			if (sec < 10)
+				_putchar('0' + sec);
+			else
+				_putchhar(sec);
+		}
+		_putchar('\n');
+	}
+}
